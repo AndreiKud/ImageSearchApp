1366bddc1c64dbdcf6de56ede31cb58435fb1650
Andrei <andreikudrya1995@gmail.com>
2021-09-11 20:23:09 +0500

    Navigation setup.

---
+++ app/src/main/java/ru/andreikud/imagesearchapp/ui/fragments/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package ru.andreikud.imagesearchapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import ru.andreikud.imagesearchapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
