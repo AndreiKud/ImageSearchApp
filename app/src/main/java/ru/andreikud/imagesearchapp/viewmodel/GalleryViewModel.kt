f70101dcf2ed92829db7981ec38bcaebe3b87350
Andrei <andreikudrya1995@gmail.com>
2021-09-12 16:36:32 +0500

    Query loss after process death fix.

--- app/src/main/java/ru/andreikud/imagesearchapp/viewmodel/GalleryViewModel.kt
+++ app/src/main/java/ru/andreikud/imagesearchapp/viewmodel/GalleryViewModel.kt
@@ -2,6 +2,8 @@ package ru.andreikud.imagesearchapp.viewmodel
 
 import androidx.lifecycle.*
 import androidx.paging.cachedIn
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
@@ -13,13 +15,18 @@ import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
-    private val unsplashRepository: UnsplashRepository
+    private val unsplashRepository: UnsplashRepository,
+    private val state: SavedStateHandle
 ) : ViewModel() {
 
-    val searchQuery = MutableLiveData("cats")
+    val searchQuery = state.getLiveData(QUERY_KEY, DEFAULT_QUERY)
 
     val photos = searchQuery.switchMap { currentQuery ->
         unsplashRepository.getSearchResults(currentQuery).cachedIn(viewModelScope)
     }
 
+    companion object {
+        private const val DEFAULT_QUERY = "cats"
+        private const val QUERY_KEY = "query_key"
+    }
 }
